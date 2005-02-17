0fc4617dc52a482ce602cc7418bb877e853fecfc
Vincent Lascaux <vincentlascaux@php.net>
2005-02-17 20:54:37 +0000

    Change from Predicat to Predicate
    git-svn-id: https://svn.php.net/repository/pear/packages/File_Archive/trunk@180002 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ source/Archive/Predicate/Custom.php
@@ -0,0 +1,60 @@
+<?
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330,Boston,MA 02111-1307 USA
+ *
+ * @category   File Formats
+ * @package    File_Archive
+ * @author     Vincent Lascaux <vincentlascaux@php.net>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.gnu.org/copyleft/lesser.html  LGPL
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/File_Archive
+ */
+
+require_once "File/Archive/Predicate.php";
+
+/**
+  * Custom predicate built by supplying a string expression
+  *
+  * Example:
+  *     new File_Archive_Predicate_Custom("return strlen($source->getFilename())<100;")
+  *     new File_Archive_Predicate_Custom("strlen($source->getFilename())<100;")
+  *     new File_Archive_Predicate_Custom("strlen($source->getFilename())<100")
+  *
+  * @see        File_Archive_Predicate File_Archive_Reader_Filter
+  */
+class File_Archive_Predicate_Custom extends File_Archive_Predicate
+{
+    var $expression;
+
+    function File_Archive_Predicate_Custom($expression)
+    {
+        $this->expression = $expression.";";
+        if(strpos($this->expression, "return") === false) {
+            $this->expression = "return ".$this->expression;
+        }
+
+    }
+    function isTrue($source)
+    {
+        return eval($this->expression);
+    }
+}
+
+?>
