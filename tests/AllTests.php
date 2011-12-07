c88e18d4643dbeb906d49dbf8e871a4c3d24e813
Daniel Convissor <danielc@php.net>
2011-12-07 18:12:45 +0000

    Upgrade PHPUnit require statements. (Bug #19108)
    git-svn-id: https://svn.php.net/repository/pear/packages/File_Archive/trunk@320595 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,8 +3,14 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'File_Archive_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+if ($fp = @fopen('PHPUnit/Autoload.php', 'r', true)) {
+    require_once 'PHPUnit/Autoload.php';
+} elseif ($fp = @fopen('PHPUnit/Framework.php', 'r', true)) {
+    require_once 'PHPUnit/Framework.php';
+} else {
+    die("skip could not find PHPUnit");
+}
+fclose($fp);
 
 require_once dirname(__FILE__) . '/FileArchiveTest.php';
 
